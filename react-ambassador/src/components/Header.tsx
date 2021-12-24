ac36c34ff9dc66e88af1baecd3b9ce6691b6d03b
hidetomo0102 <hidenorishio@icloud.com>
2021-12-25 02:17:40 +0900

    create Header

--- react-ambassador/src/components/Header.tsx
+++ react-ambassador/src/components/Header.tsx
@@ -1,8 +1,37 @@
-import React, { useState } from "react";
+import { FC, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { User } from "../models/user";
 
-export const Header = () => {
+interface Props {
+  user: User;
+}
+
+export const Header: FC<Props> = (props: Props) => {
   const [title, setTitle] = useState("Welcome");
   const [description, setDescription] = useState("Share links");
+
+  const { user } = props;
+
+  let buttons;
+  if (!user.id) {
+    buttons = (
+      <p>
+        <Link to="/login" className="btn btn-primary my-2">
+          Login
+        </Link>
+        <Link to="/register" className="btn btn-secondary my-2">
+          Register
+        </Link>
+      </p>
+    );
+  }
+
+  useEffect(() => {
+    if (user.id) {
+      setTitle(`$${user.revenue}`);
+      setDescription("Your earnings so far");
+    }
+  });
 
   return (
     <section className="py-5 text-center container">
@@ -10,7 +39,7 @@ export const Header = () => {
         <div className="col-lg-6 col-md-8 mx-auto">
           <h1 className="fw-light">{title}</h1>
           <p className="lead text-muted">{description}</p>
-          {`buttons`}
+          {buttons}
         </div>
       </div>
     </section>
